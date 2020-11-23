8c1f4d63b0f3683e55ac29e4552c3a6c9364eb7d
Ameetesh Sharma <ameeteshsharma@Ameeteshs-MacBook-Air.local>
2020-11-23 14:17:17 +0530

    added assertion

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,31 +7,10 @@ class StringCalculatorTest {
 	@Test
 	void test1() {
 		StringCalculator strcalc1 = new StringCalculator();
-		strcalc1.add("");
+		int act = strcalc1.add("1\n1,2");
+		int exp = 4;
+		assertEquals(exp,act);
 	}
 	
-	@Test
-	void test2() {
-		StringCalculator strcalc2 = new StringCalculator();
-		strcalc2.add("1");
-	}
-	
-	@Test
-	void test3() {
-		StringCalculator strcalc3 = new StringCalculator();
-		strcalc3.add("2");
-	}
-	
-	@Test
-	void test4() {
-		StringCalculator strcalc4 = new StringCalculator();
-		strcalc4.add("1,2");
-	}
-	
-	@Test
-	void test5() {
-		StringCalculator strcalc5 = new StringCalculator();
-		strcalc5.add("1\n1,2");
-	}
 
 }
